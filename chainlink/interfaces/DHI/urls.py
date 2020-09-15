88b1ac6745e799c96150c7e6b6444848cf54ad04
oasisMystre <oasis.mystre@gmail.com>
2020-09-15 17:18:01 +0100

    ...

--- chainlink/interfaces/DHI/urls.py
+++ chainlink/interfaces/DHI/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from wallet import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('wallet/' , views.index),
 ]
